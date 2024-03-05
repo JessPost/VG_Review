af47ea85eb808d02d754f44fc09e1d72ced9c84e
Jess <116526487+codeBlooded404@users.noreply.github.com>
2024-03-04 19:40:32 -0800

    Added Name to top

--- VG_Review/Controllers/HomeController.cs
+++ VG_Review/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using VG_Review.Areas.Identity.Data;
 using VG_Review.Models;
 
 namespace VG_Review.Controllers
@@ -7,14 +9,26 @@ namespace VG_Review.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<User> _userManager; 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index() 
         {
+            var user = await _userManager.GetUserAsync(User); 
+            if (user != null)
+            {
+                ViewBag.FirstName = user.FirstName; 
+            }
+            else
+            {
+                ViewBag.FirstName = "Guest";
+            }
+
             return View();
         }
 
